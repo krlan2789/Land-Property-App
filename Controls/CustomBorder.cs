d7fce29a515bcc857a69f7ed9c38fd36d5b7fadb
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-08-11 21:06:07 +0700

    Add CustomBorder . .

---
+++ Controls/CustomBorder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Land_Property_App.Controls
+{
+    public class CustomBorder : Border
+    {
+        private List<int> CornerValues = [10, 40, 70, 100];
+
+        public CustomBorder()
+        {
+            AddCornerRadius();
+        }
+
+        private void AddCornerRadius()
+        {
+            var index = new Random().Next(4);
+
+            StrokeShape = new RoundRectangle
+            {
+                CornerRadius = new CornerRadius(CornerValues[index])
+            };
+        }
+    }
+}
